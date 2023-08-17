f9c78472ebdb5a3e69c919991c114b8ec1c28d63
xenia <kseniakotyga@gmail.com>
2023-08-17 17:37:38 +0300

    new new commit

--- homework1/main.cpp
+++ homework1/main.cpp
@@ -3,63 +3,37 @@
 #include <string>
 
 
-void print(int number[], int size) {
-	for (int i = 0; i < size; i++) {
-		std::cout << number[i] << " " << std::endl;
-	}
-}
-
-
-void revers(int number[], int size) {
-	int value;
-	for (int i = 0; i < size; i++) {
-		value = number[i];
-		number[i] = number[size - i - 1];
-		number[size - i - 1] = value;
-	}
-}
-
-
-
-
 int main() {
 	std::ifstream file("in.txt");
-	std::string size_1_arr;
-	std::string size_2_arr;
-	std::string array1;
-	std::string array2;
+	char size_1_arr[100];
+	char size_2_arr[100];
 
-	std::getline(file, size_1_arr);
-	 std::cout << size_1_arr << std::endl;
-	 std::getline(file, array1);
-	 std::cout << array1 << std::endl;
-
-    std::getline(file, size_2_arr);
-	 std::cout << size_2_arr << std::endl;
-	 std::getline(file, array2);
-	 std::cout << array2 << std::endl;
-
-	 std::cout << std::endl;
-	 std::cout << std::endl;
-
-	
-	 int size_1 = stoi(size_1_arr);
-	 int size_2 = stoi(size_2_arr);
-	std::cout << size_1 << " " << size_2 << std::endl;
-	 int** array_1 = new int* [size_1];
-	for (int i = 0; i < size_1; i++) {
-		// ???
+	file >> size_1_arr;
+	int size1 = atoi(size_1_arr);
+	int* array1 = new int[size1];
+	for (int i = 0; i < size1; i++) {
+		file >> array1[i];
 	}
-	 
-	 
 
+	file >> size_2_arr;
+	int size2 = atoi(size_2_arr);
+	int* array2 = new int[size2];
+	for (int i = 0; i < size2; i++) {
+		file >> array2[i];
+	}
 
-  std::ofstream file_out("out,txt");
-   file_out << size_2 << std::endl;
-   file_out << " " << std::endl;
-   file_out << size_1 << std::endl;
-   file_out << " " << std::endl;
-	
+	std::ofstream file_out("out.txt ");
+	file_out << size2 << std::endl;
+	for (int i = size2 - 1; i >= 0; i--) {
+		file_out << array2[i] << " ";
+	}
+	file_out << std::endl;
+	file_out << size1 << std::endl;
+	for (int i = size1 - 1; i >= 0; i--) {
+		file_out << array1[i] << " ";
+	}
+
+	delete[] array1;
+	delete[] array2;
+
 }
-
-
